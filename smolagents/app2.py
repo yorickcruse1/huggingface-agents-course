34400575a8f1e2570bc151af4ae1d26416d03044
Yorick Cruse <yorick.cruse@lightspeedhq.com>
2025-05-21 11:23:05 -0400

    smolagent course

---
+++ smolagents/app2.py
@@ -0,0 +1,5 @@
+from smolagents import CodeAgent, DuckDuckGoSearchTool, InferenceClientModel
+
+agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=InferenceClientModel())
+
+agent.run("Search for the best music recommendations for a party at the Wayne's mansion.")
